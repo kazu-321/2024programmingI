1028ad5dbe7f8f3a850354ccc43b2610e7721a86
kazu <kzs321kzs@gmail.com>
2024-05-02 09:04:11 +0900

    課題4-8まで

---
+++ 0502/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main42(){
+    printf("\n\n課題4-2\n");
+    char a,b,c;
+    a='9';
+    b='A';
+    c='a';
+    printf("%c %d %x \n",a,a,a);
+    printf("%c %d %x \n",b,b,b);
+    printf("%c %d %x \n",c,c,c);
+    return 0;
+}
+
+
+int main46(){
+    printf("\n\n課題4-6\n");
+    int x,a,b;
+    printf("x>>>");scanf("%d",&x);
+    printf("a>>>");scanf("%d",&a);
+    printf("b>>>");scanf("%d",&b);
+    x+=1;
+    printf("2) %d\n",x);
+    a+=b;
+    x*=a;
+    printf("3) %d\n",x);
+    x/=2;
+    printf("4) %d\n",x);
+    x--;
+    printf("5) %d\n",x);
+}
+
+
+int main48(){
+    printf("\n\n課題4-8\n");
+    int x=1;
+    x<<=1;   printf("%d\n",x);
+    x<<=1;   printf("%d\n",x);
+    x<<=1;   printf("%d\n",x);
+    return 0;
+}
+
+
+int main49(){
+    printf("\n\n課題4-9\n");
+    printf("n進法からm進法に変換します。\n");
+    printf("入力は各桁ごとに改行してください。\n");
+    while(1){
+        
+    }
+}
