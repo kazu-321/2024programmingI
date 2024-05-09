fbcf05d112a5b410886eb854e62e4eaec9f7a586
kazu <kzs321kzs@gmail.com>
2024-05-09 09:24:27 +0900

    課題5-9追加

---
+++ 0509/main59.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int is_uruu(int year){ 
+    if(year%4==0){
+        if(year%100==0){
+            if(year%400==0){
+                return 1;
+            }
+            return 0;
+        }
+        return 1;
+    }
+    return 0;
+}
+
+int main(void){
+    int year;
+    printf("西暦を入力>>>");scanf("%d",&year);
+    if(is_uruu(year)) printf("%d年はうるう年です。\n",year);
+    else printf("%d年はうるう年ではありません。\n",year);
+    return 1;
+}
