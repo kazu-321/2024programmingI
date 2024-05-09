825b875eb3c451ae9b9b05521a17b313958d1914
kazu <kzs321kzs@gmail.com>
2024-05-09 09:07:06 +0900

    課題5-5追加

---
+++ 0509/main55.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int user_input;
+    printf("Q.恋人の前で血まみれになるなぁーんだ？\n");
+    printf("1. わたし\n");
+    printf("2. おまえ\n");
+    printf("3. ばなな\n");
+    printf("答えの数字を入力>>>"); scanf("%d",&user_input);
+    if(user_input==2) printf("正解\n");
+    else printf("不正解\n");
+    return 1;
+}
