87f0226ecb4332b9258df23594ece2e6e21452ec
kazu <kzs321kzs@gmail.com>
2024-05-10 15:17:34 +0900

    令和最新版

--- 0509/main55.c
+++ 0509/main55.c
@@ -2,12 +2,16 @@
 
 int main(void){
     int user_input;
+    // 人マニア
     printf("Q.恋人の前で血まみれになるなぁーんだ？\n");
     printf("1. わたし\n");
     printf("2. おまえ\n");
     printf("3. ばなな\n");
     printf("答えの数字を入力>>>"); scanf("%d",&user_input);
-    if(user_input==2) printf("ビバいい！\n");
-    else printf("ビバよくない！！\n");
+    printf("\033[4A\033[K\nA\n\033[K\n");
+    if(user_input==2){
+        printf("\033[Kビバよくない！\n");
+    }
+    else printf("\033[Kふせいかい！\n");
     return 0;
 }
