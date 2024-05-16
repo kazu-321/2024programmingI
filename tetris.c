0bfdc4d094f83afb5b0305184e7bd63a10ac7f4f
kazu <kzs321kzs@gmail.com>
2024-05-16 10:11:44 +0900

    色々追加

--- tetris.c
+++ tetris.c
@@ -4,6 +4,40 @@
 #include <termios.h>
 #include <unistd.h>
 #include <signal.h>
+
+#define clearScreen() printf("\033[2")
+#define setPosition(x,y) printf("\033[%d;%dH",(y)+1,(x)*2+1)
+#define cursolOn() printf("\033[?25h")
+#define cursolOff() printf("\033[?25l")
+#define WIDTH 10
+#define HEIGHT 20
+#define setCharColor(n) printf("\033[3%dm",(n))
+#define setBackColor(n) printf("\033[4%dm",(n))
+#define BLACK 0
+#define RED 1
+#define GREEN 2
+#define YELLOW 3
+#define BLUE 4
+#define MAGENTA 5
+#define CYAN 6
+#define WHITE 7
+#define DEFAULT 9
+#define setAttribute(n) printf("\033[%dm",(n))
+#define NORMAL 0 //通常
+#define BLIGHT 1 //明るく
+#define DIM 2 //暗く
+#define UNDERBAR 4 //下線
+#define BLINK 5 //点滅
+#define REVERSE 7 //明暗反転
+#define HIDE 8 //隠れ(見えない)
+#define STRIKE 9 //取り消し線
+typedef struct cell {
+ char c; //表示文字
+ int charcolor; //表示色
+ int backcolor; //背景色
+ int attribute; //文字属性
+} Cell;
+
 extern int errno;
 struct termios otty,ntty;
 int kbhit(void);
