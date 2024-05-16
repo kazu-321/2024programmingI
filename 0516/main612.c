a943eeaa767af0b7c53b5014f2aae58c9856cca8
kazu <kzs321kzs@gmail.com>
2024-05-16 09:12:12 +0900

    課題6-12 更新

--- 0516/main612.c
+++ 0516/main612.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main(){
-    printf("  | 1  2  3  4  5  6  7  8  9\n");
-    printf("-----------------------------\n");
-    
+    printf("   |  1  2  3  4  5  6  7  8  9\n");
+    printf("-------------------------------\n");
+    for(int i=1;i<10;i++){
+        printf(" %d |",i);
+        for(int j=1;j<10;j++){
+            printf("%3d",i*j);
+        }
+        printf("\n");
+    }
 }
