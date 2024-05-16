f3411446401a7dd37a4075285b6acb9aa6bb4c88
kazu <kzs321kzs@gmail.com>
2024-05-16 09:03:32 +0900

    課題6-8追加

---
+++ 0516/main68.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int end;
+    printf("正の整数を入力してください>>>");
+    scanf("%d",&end);
+    int sum=0;
+    for(int i=0;i<end+1;i++) sum+=i;
+    printf("1から%dまでを足した値は%dです。\n",end,sum);
+    return 0;
+}
