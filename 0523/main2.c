aa87b6f3a34f4bc2ceafcabc992e198394a12269
kazu <kzs321kzs@gmail.com>
2024-05-23 08:55:16 +0900

    課題2追加

---
+++ 0523/main2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+    int a=10,b=3;
+    printf("a=%d, b=%d\n",a,b);
+    printf("和:%d\n",a+b);
+    printf("差:%d\n",a-b);
+    printf("積:%d\n",a*b);
+    printf("商:%d\n",a/b);
+    printf("剰余:%d\n",a%b);
+    return 0;
+}
