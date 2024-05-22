33e8632f7e0114759e2592ff37aa26a106c8b68c
kazu <kzs321kzs@gmail.com>
2024-05-23 08:57:19 +0900

    課題3追加

---
+++ 0523/main3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+    int a=20;
+    printf("%d\n",a==20);
+    printf("%d\n",a>20);
+    printf("%d\n",a>=0&&a<10);
+    printf("%d\n",a>=0||a<10);
+    return 0
+}
