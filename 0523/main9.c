5a6e3adf8a4c50d0c910fda5261b756b3d9ed9c6
kazu <kzs321kzs@gmail.com>
2024-05-23 09:16:52 +0900

    typo

--- 0523/main9.c
+++ 0523/main9.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 int main(){
-    int n=15;
+    int n;
+    n=15;
     n+=10;
     printf("%d\n",n);
 
-    int n=15;
+    n=15;
     n-=20;
     printf("%d\n",n);
 
-    int n=15;
+    n=15;
     n*=4;
     printf("%d\n",n);
 
-    int n=15;
+    n=15;
     n/=10;
     printf("%d\n",n);
 
-    int n=15;
+    n=15;
     n%=6;
     printf("%d\n",n);
 
