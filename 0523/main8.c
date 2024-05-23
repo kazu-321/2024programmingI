227aad07139072287e3274450b1f1f836f4ccdf6
kazu <kzs321kzs@gmail.com>
2024-05-23 09:13:35 +0900

    課題8追加

---
+++ 0523/main8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int i;
+    for(i=2;i<6;i++){
+        printf("%d\n",i);
+    }
+    
+    i=2;
+    while(i<6){
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
