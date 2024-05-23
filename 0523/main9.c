1580226e2f127a15e9612517281c86807ae6172d
kazu <kzs321kzs@gmail.com>
2024-05-23 09:16:00 +0900

    課題9追加

---
+++ 0523/main9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int n=15;
+    n+=10;
+    printf("%d\n",n);
+
+    int n=15;
+    n-=20;
+    printf("%d\n",n);
+
+    int n=15;
+    n*=4;
+    printf("%d\n",n);
+
+    int n=15;
+    n/=10;
+    printf("%d\n",n);
+
+    int n=15;
+    n%=6;
+    printf("%d\n",n);
+
+    return 0;
+}
