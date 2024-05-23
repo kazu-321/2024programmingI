281bb1743cfb60380a860a5df27ccd870fe5f68e
kazu <kzs321kzs@gmail.com>
2024-05-23 09:11:38 +0900

    typo

--- 0523/main7.c
+++ 0523/main7.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     int x;
-    scanf("%d".&x);
+    scanf("%d",&x);
     switch(x){
         case 1: printf("one");   break;
         case 2: printf("two");   break;
