99d32716da20e68101ba10c5a7e126a84620dd14
kazu <kzs321kzs@gmail.com>
2024-05-23 09:14:19 +0900

    課題8実行

--- 0523/main8.c
+++ 0523/main8.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main(){
     int i;
+    printf("for---\n");
     for(i=2;i<6;i++){
         printf("%d\n",i);
     }
     
+    printf("while---\n");
     i=2;
     while(i<6){
         printf("%d\n",i);
