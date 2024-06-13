16e93cccc7e3746a5efe9a850084ab411e3026ed
kazu <kzs321kzs@gmail.com>
2024-06-13 09:05:14 +0900

    課題9-7更新

--- 0613/main97.c
+++ 0613/main97.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
 int is_prime(int n) {
-    if (n < 2) {
-        return 0;
-    }
-    for (int i = 2; i*i < n; i++) {
-        if (n % i == 0) {
-            return 0;
-        }
+    if(n < 2) return 0;         // 偶数判定でループ回数を半分にできる
+    for(int i=2;i*i<= n;i++) {  // 約数の最小値は√nなのでi^2<=nまででよい
+        if(n % i == 0) return 0;
     }
     return 1;
 }
 
 int main() {
     for(int i = 2; i < 1000; i++) {
-        if (is_prime(i)) {
-            printf("%d ", i);
-        }
+        if (is_prime(i)) printf("%d ", i);
     }
     return 0;
 }
