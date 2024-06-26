7dbd648216e1c2537fea901c2c3f1b5369cb7560
kazu <kzs321kzs@gmail.com>
2024-06-27 08:53:18 +0900

    typo

--- 0627/main114.c
+++ 0627/main114.c
@@ -2,8 +2,8 @@
 int main(){
     int data[5];
     for(int i=0;i<5;i++) scanf("%d",data+i);
-    int sun;
-    for(int i=0;i<5;i++) sun += data[i];
+    int sum=0;
+    for(int i=0;i<5;i++) sum += data[i];
     printf("合計:%d\n",sum);
     printf("平均:%f\n",sum/5.0);
     return 0;
