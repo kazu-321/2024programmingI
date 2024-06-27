65a599525fd90d67e8e100ba549a270dc0a0c476
kazu <kzs321kzs@gmail.com>
2024-06-27 09:14:57 +0900

    課題11-10追加

---
+++ 0627/main1110.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char gm[]="おはよう";
+    char hl[]="こんにちは";
+    char gn[]="おやすみなさい";
+    printf("%s\n", gm);
+    printf("%s\n", hl);
+    printf("%s\n", gn);
+}
